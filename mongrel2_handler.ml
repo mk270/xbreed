9bdac2da2eb624fed5603922ac28fbd04e1d38c3
Martin Keegan <martin@no.ucant.org>
2013-06-14 04:53:37 +0100

    reindent

--- mongrel2_handler.ml
+++ mongrel2_handler.ml
@@ -16,68 +16,68 @@ open Pcre
 
 module Dispatcher = struct
 
-let generic_response body code status =
-	Lwt.return {
-		m2resp_body = body;
-		m2resp_code = code;
-		m2resp_status = status;
-		m2resp_headers = [("Content-type", "text/html")];
-	}
+	let generic_response body code status =
+		Lwt.return {
+			m2resp_body = body;
+			m2resp_code = code;
+			m2resp_status = status;
+			m2resp_headers = [("Content-type", "text/html")];
+		}
 
-let serve_from_file filename hreq =
-	let headers = [("Content-type", "text/html")] in
-
-	let restructure_thingy text = {
-		m2resp_body = text;
-		m2resp_code = 200;
-		m2resp_status = "OK";
-		m2resp_headers = headers;
-	} in
-
-	try_lwt let page_text =
-				Lwt_io.with_file ~mode:Lwt_io.Input filename Lwt_io.read
-			in
-				restructure_thingy =|< page_text
+	let serve_from_file filename hreq =
+		let headers = [("Content-type", "text/html")] in
+			
+		let restructure_thingy text = {
+			m2resp_body = text;
+			m2resp_code = 200;
+			m2resp_status = "OK";
+			m2resp_headers = headers;
+		} in
+			
+		try_lwt let page_text =
+					Lwt_io.with_file ~mode:Lwt_io.Input filename Lwt_io.read
+				in
+					restructure_thingy =|< page_text
 	with 
 		| Unix.Unix_error (Unix.ENOENT, _, _) ->
 			generic_response "File not found" 404 "Not Found"
 		| _ -> generic_response "Internal server error" 500 "Internal Server Error"
 
-let respond hreq = serve_from_file "/etc/services" hreq
+	let respond hreq = serve_from_file "/etc/services" hreq
 
-let normal_document s = generic_response s 200 "OK"
+	let normal_document s = generic_response s 200 "OK"
 
-let handler1 request matched_args =	normal_document "MASH"
+	let handler1 request matched_args =	normal_document "MASH"
 
-let handler2 request matched_args =
-	respond request
+	let handler2 request matched_args =
+		respond request
 
-let handler3 request matched_args =
-	let uri = List.assoc "URI" request.m2req_headers in
-	let filename = "." ^ uri in
-		serve_from_file filename request
-
-let not_found request matched_args =
-	generic_response "Not found" 404 "Not Found"
-
-let dispatch handlers handle_404 request =
-	let matches pat =
+	let handler3 request matched_args =
 		let uri = List.assoc "URI" request.m2req_headers in
-			Pcre.extract_all ~pat uri
-	in
-		
-	let rec handle = function
-		| [] -> handle_404 request [||]
-		| (url_regexp, handler) :: tl ->
-			try_lwt let args = matches url_regexp in
-						handler request args
-			with Not_found ->
-				handle tl
-	in
-		handle handlers
+		let filename = "." ^ uri in
+			serve_from_file filename request
 
-let make handlers not_found =
-	dispatch handlers not_found
+	let not_found request matched_args =
+		generic_response "Not found" 404 "Not Found"
+
+	let dispatch handlers handle_404 request =
+		let matches pat =
+			let uri = List.assoc "URI" request.m2req_headers in
+				Pcre.extract_all ~pat uri
+		in
+		
+		let rec handle = function
+			| [] -> handle_404 request [||]
+			| (url_regexp, handler) :: tl ->
+				try_lwt let args = matches url_regexp in
+							handler request args
+	            with Not_found ->
+					handle tl
+		in
+			handle handlers
+
+	let make handlers not_found =
+		dispatch handlers not_found
 
 end
 

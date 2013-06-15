c60ad6fea8961a5c158a5a413944221ca6636516
Martin Keegan <martin@no.ucant.org>
2013-06-15 19:06:51 +0100

    rm unused

--- src/layout.ml
+++ src/layout.ml
@@ -39,29 +39,6 @@ let yaml_frontmatter s =
 	let layout = List.assoc "layout" kvps in
 		(layout, rest)
 
-let json_string_of_file filename =
-	Util.file_contents filename >|=
-	json_of_s >|=
-	Yojson.Safe.to_string
-
-let interpolate replacement template =
-	let needle = "<!-- # MAGIC # -->" in
-	let replacement = "params = " ^ replacement ^ ";\n" in
-		Str.replace_first (Str.regexp needle) replacement template
-
-let _make_layout template_name docroot filename =
-	let template_name = Util.path_join [docroot; "_layouts"; template_name] in
-
-	let modify_template json_string =
-		Util.file_contents template_name >|=
-		interpolate json_string
-	in
-		json_string_of_file filename >>=
-		modify_template
-
-let make_layout docroot filename =
-	_make_layout "layout.html" docroot filename
-
 let interpolate_contents contents template =
 	let rex = Pcre.regexp "{{ ?contents ?}}" in
 		Pcre.replace ~rex ~templ:contents template

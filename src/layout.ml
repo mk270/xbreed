beda37f62bf8d92255571369aee30c81e25bdc9c
Martin Keegan <martin@no.ucant.org>
2013-06-15 19:03:31 +0100

    redo markdown processing

--- src/layout.ml
+++ src/layout.ml
@@ -12,8 +12,32 @@
 
 open Yojson.Safe
 open Lwt
+open Pcre
+
+exception Frontmatter_not_found
 
 let json_of_s s = `Assoc [ ("content", `String s) ]
+
+let yaml_frontmatter s =
+	let kvp line =
+		let rex = Pcre.regexp ": ?" in
+			match (Pcre.split ~max:2 ~rex line) with
+				| [k; v] -> (k, v)
+				| _ -> raise Frontmatter_not_found
+	in
+	let rex = Pcre.regexp  ~flags:[`MULTILINE] "^---\n" in
+	let max = 3 in
+	let sections s = Pcre.split ~max ~rex s in
+	let yaml_assoc = function
+		| [""; yaml; rest] ->
+			let lines = Str.split (Str.regexp "\n") yaml in
+			let kvps = List.map kvp lines in
+				(kvps, rest)
+		| _ -> raise Frontmatter_not_found
+	in
+	let kvps, rest = yaml_assoc (sections s) in
+	let layout = List.assoc "layout" kvps in
+		(layout, rest)
 
 let json_string_of_file filename =
 	Util.file_contents filename >|=
@@ -37,4 +61,20 @@ let _make_layout template_name docroot filename =
 
 let make_layout docroot filename =
 	_make_layout "layout.html" docroot filename
-		
+
+let interpolate_contents contents template =
+	let rex = Pcre.regexp "{{ ?contents ?}}" in
+		Pcre.replace ~rex ~templ:contents template
+
+let apply_template docroot (layout, body) = 
+	let layout_file = layout ^ ".html" in
+	let rendered_body = Util.html_of_markdown body in
+	let template_name = Util.path_join [docroot; "_layouts"; layout_file] in
+		Util.file_contents template_name >|=
+			interpolate_contents rendered_body
+
+let make_layout docroot filename =
+	Util.file_contents filename >|=
+	yaml_frontmatter >>=
+	apply_template docroot
+				

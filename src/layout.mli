c9982d0b942a463c3a36542ad70eb07c1db3f4e9
Martin Keegan <martin@no.ucant.org>
2013-06-15 14:56:22 +0100

    add (c) boilerplate

--- src/layout.mli
+++ src/layout.mli
@@ -1,2 +1,13 @@
 
+(*
+  XBreed, a Mongrel2 handler for OCaml
+
+  Copyright (C) 2013  Martin Keegan
+
+  This programme is free software; you may redistribute and/or modify
+  it under the terms of the Apache License v2.0
+*)
+
+(* this code isn't even supposed to be alpha quality - it's just a hack ATM *)
+
 val make_layout : string -> string -> string Lwt.t
